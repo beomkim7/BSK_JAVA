6d4cf49fd5c197af84788a7acadcbbe4c6d318f1
beomkim7 <beomkim7@naver.com>
2023-12-05 11:41:12 +0900

    print

--- src/com/winter/study1/Study1For.java
+++ src/com/winter/study1/Study1For.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Study1For {
 	public static void main(String [] args) {
+		System.out.println("프로그램 시작");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수하나를 입력하세요");
 		
 		int num = sc.nextInt();
 		
 		//입력받은 정수의 약수를 출력하세요
-		
-		for(int i =1 ; i <= num/2;i++)
+		//for(입력식;조건식;증감식){}
+		for(int i = 1 ; i <= num/2;i++)
 		{
-			if(num%i==0) {
+			if(num%i == 0) {
 				System.out.println(i);
 				
 			}
 		}System.out.println(num);
 		
-		
-		
+				
 	}
 }
