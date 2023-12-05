f02269ab2dea5510b785db9b51e9ab949aa594d5
beomkim7 <beomkim7@naver.com>
2023-12-05 14:16:28 +0900

    sec

--- src/com/winter/study1/Study1For.java
+++ src/com/winter/study1/Study1For.java
@@ -10,16 +10,24 @@ public class Study1For {
 		
 		int num = sc.nextInt();
 		
+		int a =1;
 		//입력받은 정수의 약수를 출력하세요
+		//약수의 갯수를 출력하세요
 		//for(입력식;조건식;증감식){}
 		for(int i = 1 ; i <= num/2;i++)
 		{
 			if(num%i == 0) {
 				System.out.println(i);
 				
+				a++;
+				
 			}
-		}System.out.println(num);
-		
+			
+				
+			
+		}
+		System.out.println(num);
+		System.out.println(a);
 				
 	}
 }
