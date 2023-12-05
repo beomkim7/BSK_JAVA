25a1ef8ec3bb283db5fa62cff545e2c7aaa2c337
beomkim7 <beomkim7@naver.com>
2023-12-05 15:42:59 +0900

    1

--- src/com/winter/study1/Study4For.java
+++ src/com/winter/study1/Study4For.java
@@ -9,43 +9,46 @@ public class Study4For {
 		
 		//1. 단발모드
 		//2. 점사모드
-		
-	
-		
 		Scanner sc = new Scanner(System.in);
 		
-		int one;
+
 		
-		for(int i = 1 ; i <= 3 ; i++) {
 		
-			
-			
-				
-				one = sc.nextInt();
-				System.out.println("1. 단발모드");
-				
-				System.out.println("2. 점사모드");
-				
-				System.out.println("3. 연사모드");
-			
-					if(one == 1) {
-						for(int j =1;j<=30;j++) {
-							System.out.println("탕");}
-					}
-					if(one == 2) {
-						for(int j =1 ; j<=10 ; j++) {
-					
-					System.out.println("탕탕탕");
-					}}
-					if(one == 3) {
-						for(int j = 1 ; j <=30 ; j++) {
-							System.out.print("탕");
-							
-						}System.out.println("");
-					}
-					if(i<=2) {System.out.println(i+"탄 소모");}
-					else {System.out.println("마지막 탄창 소모");}
-			}
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int one;
+//		
+//		for(int i = 1 ; i <= 3 ; i++) {
+//		
+//			
+//			
+//				
+//				one = sc.nextInt();
+//				System.out.println("1. 단발모드");
+//				
+//				System.out.println("2. 점사모드");
+//				
+//				System.out.println("3. 연사모드");
+//			
+//					if(one == 1) {
+//						for(int j =1;j<=30;j++) {
+//							System.out.println("탕");}
+//					}
+//					if(one == 2) {
+//						for(int j =1 ; j<=10 ; j++) {
+//					
+//					System.out.println("탕탕탕");
+//					}}
+//					if(one == 3) {
+//						for(int j = 1 ; j <=30 ; j++) {
+//							System.out.print("탕");
+//							
+//						}System.out.println("");
+//					}
+//					if(i<=2) {System.out.println(i+"탄창 소모");}
+//					else {System.out.println("마지막 탄창 소모");}
+//			}
 			
 			
 			
