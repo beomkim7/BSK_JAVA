b94d80260d8d990c1ae75ffb0aa7703bb844ebed
beomkim7 <beomkim7@naver.com>
2023-12-05 15:40:25 +0900

    done

--- src/com/winter/study1/Study4For.java
+++ src/com/winter/study1/Study4For.java
@@ -1,8 +1,54 @@
 package com.winter.study1;
 
+import java.util.Scanner;
+
 public class Study4For {
 	public static void main(String[] args) {
-		System.out.println("master");
-		System.out.println("Add S4");
+		//서든 FPS
+		//총알 : 30발 탄창 * 3
+		
+		//1. 단발모드
+		//2. 점사모드
+		
+	
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int one;
+		
+		for(int i = 1 ; i <= 3 ; i++) {
+		
+			
+			
+				
+				one = sc.nextInt();
+				System.out.println("1. 단발모드");
+				
+				System.out.println("2. 점사모드");
+				
+				System.out.println("3. 연사모드");
+			
+					if(one == 1) {
+						for(int j =1;j<=30;j++) {
+							System.out.println("탕");}
+					}
+					if(one == 2) {
+						for(int j =1 ; j<=10 ; j++) {
+					
+					System.out.println("탕탕탕");
+					}}
+					if(one == 3) {
+						for(int j = 1 ; j <=30 ; j++) {
+							System.out.print("탕");
+							
+						}System.out.println("");
+					}
+					if(i<=2) {System.out.println(i+"탄 소모");}
+					else {System.out.println("마지막 탄창 소모");}
+			}
+			
+			
+			
 	}
+
 }
