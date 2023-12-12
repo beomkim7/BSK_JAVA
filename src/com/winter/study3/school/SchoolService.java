53cc18e6265f8fac00859bada39d9b2164834de4
beomkim7 <beomkim7@naver.com>
2023-12-12 09:37:58 +0900

    24

--- src/com/winter/study3/school/SchoolService.java
+++ src/com/winter/study3/school/SchoolService.java
@@ -3,16 +3,11 @@ package com.winter.study3.school;
 import java.util.Scanner;
 
 public class SchoolService {
-	
+	Scanner sc = new Scanner(System.in);
 	//addStudent
 	//이름,번호,점수를 입력받아서 기존 배열에 추가한 효과
-	
-	
-	//findByNum
-	//학생번호를 입력받아서, 일치하는 학생이 있는지 검색
-	//해당학생을 리턴 없으면 null리턴
 	public Student [] addStudent(Student [] students) {
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
 		Student student = new Student();
 		System.out.println("이름을 입력");
 		student.name=sc.next();
@@ -32,10 +27,30 @@ public class SchoolService {
 		
 	}
 	
+	//findByNum
+	//학생번호를 입력받아서, 일치하는 학생이 있는지 검색
+	//해당학생을 리턴 없으면 null리턴
+	public Student findByNum(Student [] students) {		
+		
+//		Scanner sc = new Scanner(System.in);
+		System.out.println("검색할 학생 번호 입력");
+		int num = sc.nextInt();
+		Student student = null;
+		for(int i = 0 ; i < students.length ; i++) {
+			
+		if(num==students[i].num) {
+			//return students[i];
+			student = students[i];
+			break;
+			}
+		}
+		return student;
+	}
+	
 	//매서드 makeStudents
 	//학생수를 입력받아서 수만큼 학생정보를 입력
 	public Student[] makeStudents() {
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
 		System.out.println("학생수를 입력하세요");
 		int count = sc.nextInt();
 		
@@ -53,21 +68,7 @@ public class SchoolService {
 			}		
 		return students;
 		}	
-	public Student findByNum(Student [] students) {		
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("검색할 학생 번호 입력");
-		int num = sc.nextInt();
-		Student student = null;
-		for(int i = 0 ; i < students.length ; i++) {
-			
-		if(num==students[i].num) {
-			//return students[i];
-			student = students[i];
-			break;
-			}
-		}
-		return student;
-	}
+
+	
 	
 }
